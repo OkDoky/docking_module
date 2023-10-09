b5e9ca3eed8c8a1ffb09ad777cb67173893db5ec
OkDoky <ie5630jk@naver.com>
2023-10-09 18:44:05 +0900

    tested

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get upgrade -y
 name_ros_version=${name_ros_version:="noetic"}
 
 # install default dependencies
-apt-get install -y git apt-utils build-essential software-properties-common
+apt-get install -y git apt-utils build-essential software-properties-common wget
 
 source /opt/ros/${name_ros_version}/setup.bash
 
@@ -14,10 +14,12 @@ cd ~/catkin_ws/src && catkin_init_workspace
 git clone https://github.com/OkDoky/docking_module.git -b master
 
 ## install IPOPT
+cd ~/
+
 wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.14.4.tar.gz
 tar -xvzf Ipopt-3.14.4.tar.gz 
-cd Ipopt-releases-3.14.4/
-apt install cppad gfortran
+cd ~/Ipopt-releases-3.14.4/
+apt-get install -y cppad gfortran liblapack-dev
 git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps/
 ./get.Mumps 
@@ -26,15 +28,13 @@ make -j4 && make test && make install
 
 cd ..
 ./configure --prefix=/usr/local --with-mumps-lib="-L/usr/local/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lcamseq" --with-mumps-incdir="/usr/local/include" ADD_CXXFLAGS="-I/usr/local/include"
-make -j4
-make test
-make install
+make -j4 && make test && make install
 
 ## cause cppad use coin/IpIpoptAppication.hpp
 mv /usr/local/include/coin-or /usr/local/include/coin
 
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib'>> ~/.bashrc
-soruce ~/.bashrc
+source ~/.bashrc
 
 # install wstool, rosdep and install & initialize dependencies packages
 apt-get install -y python3-rosdep python3-wstool python3-rosinstall python3-rosinstall-generator python3-pip
@@ -62,3 +62,4 @@ echo 'export ROS_MASTER_URI=http://localhost:11311' >> ~/.bashrc
 
 source ~/.bashrc
 cd ~/catkin_ws && catkin_make -DCMAKE_BUILD_TYPE=Release
+source ~/.bashrc
