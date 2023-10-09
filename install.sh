e8f1d58c453bd9fd3b5a7b570ee4adc3898b4577
OkDoky <ie5630jk@naver.com>
2023-10-09 16:23:08 +0900

    add install Ipopt

--- install.sh
+++ install.sh
@@ -13,8 +13,32 @@ mkdir -p /root/catkin_ws/src
 cd ~/catkin_ws/src && catkin_init_workspace
 git clone https://github.com/OkDoky/docking_module.git -b master
 
+## install IPOPT
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.14.4.tar.gz
+tar -xvzf Ipopt-3.14.4.tar.gz 
+cd Ipopt-releases-3.14.4/
+apt install cppad gfortran
+git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
+cd ThirdParty-Mumps/
+./get.Mumps 
+./configure
+make -j4 && make test && make install
+
+cd ..
+./configure --prefix=/usr/local --with-mumps-lib="-L/usr/local/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lcamseq" --with-mumps-incdir="/usr/local/include" ADD_CXXFLAGS="-I/usr/local/include"
+make -j4
+make test
+make install
+
+## cause cppad use coin/IpIpoptAppication.hpp
+mv /usr/local/include/coin-or /usr/local/include/coin
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib'>> ~/.bashrc
+soruce ~/.bashrc
+
 # install wstool, rosdep and install & initialize dependencies packages
-apt-get install -y python3-rosdep python3-wstool python3-rosinstall python3-rosinstall-generator
+apt-get install -y python3-rosdep python3-wstool python3-rosinstall python3-rosinstall-generator python3-pip
+pip3 install matplotlib
 cd ~/catkin_ws/src
 wstool init
 wstool merge ./docking_module/.rosinstall
