8efcc74c103b81c3f31ca24f9075e0e48e0c50be
OkDoky <ie5630jk@naver.com>
2024-01-28 16:45:50 +0900

    add back docking planner

--- docking_planner/src/transform.py
+++ docking_planner/src/transform.py
@@ -11,8 +11,8 @@ from copy import copy
 from tf2_msgs.msg import TFMessage
 from nav_msgs.msg import Odometry, Path
 from std_msgs.msg import String
-from geometry_msgs.msg import PointStamped, QuaternionStamped, Point, Quaternion, PoseStamped, TransformStamped, Pose2D
-from tf.transformations import quaternion_matrix, quaternion_from_matrix
+from geometry_msgs.msg import PolygonStamped, Point32, Point, Quaternion, PoseStamped, TransformStamped, Pose2D
+from tf.transformations import quaternion_matrix, quaternion_from_matrix, rotation_matrix
 sys.path.append(os.path.dirname(__file__))
 print(os.path.dirname(__file__))
 
@@ -35,6 +35,18 @@ class CallbackManager:
 
 robot_odom = Odometry()
 
+def normalizeAngle(angle, min, max):
+    if angle > max:
+        angle -= 2 * math.pi
+    elif angle < min:
+        angle += 2 * math.pi
+    return angle
+
+def normalizeAngles(angles, min_angle, max_angle):
+    angles = np.where(angles > max_angle, angles - 2 * math.pi, angles)
+    angles = np.where(angles < min_angle, angles + 2 * math.pi, angles)
+    return angles
+
 def is_near_target_value(target, value, epsilon=1e-9):
     return abs(target-value) < epsilon
 
@@ -55,23 +67,26 @@ def has_cross_line(a, b, c):
     Returns:
         Bool : return True if robot cross the line start with straight
     """
-    ab = Pose2D(x=b.x - a.x, y=b.y - a.y, theta=0.0)
+    ba = Pose2D(x=a.x - b.x, y=a.y - b.y, theta=0.0)
     bc = Pose2D(x=c.x - b.x, y=c.y - b.y, theta=0.0)
 
-    dot_prod = dot_product(ab, bc)
-    angle = math.acos(dot_prod / (magnitude(ab) * magnitude(bc)))
+    dot_prod = dot_product(ba, bc)
+    angle = math.acos(dot_prod / (magnitude(ba) * magnitude(bc)))
 
     angle_deg = math.degrees(angle)
-    return angle_deg > 90.
+    # print("[Transform] angle deg : %s"%abs(angle_deg))
+    return abs(angle_deg) < 90.
 
 def move_pose(target_pose, dist=0.1):
     dx = math.cos(target_pose.theta) * dist
     dy = math.sin(target_pose.theta) * dist
     
     result_pose = Pose2D()
-    result_pose.x = target_pose.x - dx
-    result_pose.y = target_pose.y - dy
+    result_pose.x = target_pose.x + dx
+    result_pose.y = target_pose.y + dy
     result_pose.theta = target_pose.theta
+    # print("[MOvePOse] target pose : (%s, %s), result : (%s, %s)"%(target_pose.x, target_pose.y,
+    #                                              result_pose.x, result_pose.y))
     return result_pose
 
 def find_nearest_point_on_line(line_start, line_end, pos):
@@ -88,14 +103,27 @@ def find_nearest_point_on_line(line_start, line_end, pos):
     nearest_point = Pose2D(x=nrp[0], y=nrp[1], theta=nrp[2])
     return nearest_point
 
+def debug_has_cross_line(a,b,c):
+    global polygon_pub
+    poly = PolygonStamped()
+    poly.header.stamp = rospy.Time.now()
+    poly.header.frame_id = "odom"
+    pa = Point32(x=a.x, y=a.y, z=0.0)
+    pb = Point32(x=b.x, y=b.y, z=0.0)
+    pc = Point32(x=c.x, y=c.y, z=0.0)
+    poly.polygon.points.append(pa)
+    poly.polygon.points.append(pb)
+    poly.polygon.points.append(pc)
+    polygon_pub.publish(poly)
+    
 def compute_plan(start_pose,  end_pose, 
                  straight_end, frame, sv, ev, dt=0.1):
     """_summary_
 
     Args:
-        start_point (Point): current pose of robot - odom frame
-        end_point (Point): pose of end of polynomial plan - odom frame
-        straight_end (Point): target pose(end of last path) - odom frame
+        start_pose (Pose2D): current pose of robot - odom frame
+        end_pose (Pose2D): pose of end of polynomial plan - odom frame
+        straight_end (Pose2D): target pose(end of last path) - odom frame
         frame (string): 
         sv (float): starting velocity(current velocity)
         ev (float): velocity with tracking linear path
@@ -104,11 +132,14 @@ def compute_plan(start_pose,  end_pose,
     Returns:
         Path: calculated path
     """
+    global heading_front, debug_mode
     
     if abs(sv) < 0.01:
         sv = 0.01
-    moved_end_pose = move_pose(end_pose, dist=0.05)
-    _has_cross_line = has_cross_line(start_pose, moved_end_pose, end_pose)
+    if debug_mode:
+        debug_has_cross_line(start_pose, end_pose, straight_end)
+    end_pose = move_pose(end_pose, dist=0.05)
+    _has_cross_line = has_cross_line(start_pose, end_pose, straight_end)
     path = Path()
     path.header.stamp = rospy.Time.now()
     path.header.frame_id = frame
@@ -116,9 +147,15 @@ def compute_plan(start_pose,  end_pose,
     if not _has_cross_line:
         rospy.logdebug("[Transform] Didn't Cross the Line")
         time, x, y, yaw, v, a, j = quintic_polynomials_planner(\
-            start_pose.x, start_pose.y, start_pose.theta, 
-            end_pose.x, end_pose.y, end_pose.theta, dt, 
-            sv=sv, gv=ev, max_accel=0.2)
+            start_pose.x, start_pose.y, 
+            start_pose.theta if heading_front else normalizeAngle(start_pose.theta+math.pi, -math.pi, math.pi),
+            end_pose.x, end_pose.y, 
+            end_pose.theta, 
+            dt, sv=sv, gv=ev, max_accel=0.2)
+        # if not heading_front:
+        #     yaw = np.array(yaw)
+        #     yaw = normalizeAngles(yaw+math.pi, -math.pi, math.pi).tolist()
+            
         for i, t in enumerate(time):
             pose_stamped = PoseStamped()
             pose_stamped.header.stamp = path.header.stamp + rospy.Duration.from_sec(t)
@@ -126,8 +163,8 @@ def compute_plan(start_pose,  end_pose,
             pose_stamped.pose.position.x = x[i]
             pose_stamped.pose.position.y = y[i]
             pose_stamped.pose.position.z = 0
-            rotation_matrix = tf.transformations.rotation_matrix((yaw[i]), (0, 0, 1))
-            result_q = tf.transformations.quaternion_from_matrix(rotation_matrix)
+            _rotation_matrix = rotation_matrix((yaw[i]), (0, 0, 1))
+            result_q = quaternion_from_matrix(_rotation_matrix)
             quaternion = Quaternion()
             quaternion.x = result_q[0]
             quaternion.y = result_q[1]
@@ -147,8 +184,8 @@ def compute_plan(start_pose,  end_pose,
     z_points = np.linspace(line_start[2], line_end[2], dist)
     # points = np.linspace(line_start, line_end, dist)
     points = np.vstack((x_points, y_points, z_points)).T
-    rot = tf.transformations.rotation_matrix((end_pose.theta), (0, 0, 1))
-    linear_q = tf.transformations.quaternion_from_matrix(rot)
+    rot = rotation_matrix((end_pose.theta), (0, 0, 1))
+    linear_q = quaternion_from_matrix(rot)
     for point in points:
         pose_stamped = PoseStamped()
         pose_stamped.header.frame_id = frame
@@ -179,7 +216,7 @@ def callback_odomTF(sub_odom):
     
 def callback_tracking_state(msg):
     global backup_pose, tracking_state
-    global path_displacement, robot_size, marker_displacement
+    global path_displacement, robot_size, marker_displacement, heading_front
     rospy.logwarn("[TrackingStateCallback] %s"%msg.data)
     if not msg.data == tracking_state:
         tracking_state = msg.data
@@ -188,32 +225,9 @@ def callback_tracking_state(msg):
             path_displacement = float(rospy.get_param("~path_displacement"))
             robot_size = float(rospy.get_param("~robot_size"))
             marker_displacement = float(rospy.get_param("~marker_displacement"))
-
-def get_rotation_angle(p1, p2):
-    v = (p2[0]-p1[0], p2[1]-p1[1])
-    # 벡터의 노름 계산
-    norm_v = math.sqrt(v[0]**2 + v[1]**2)
-    # 단위 벡터로 변환
-    unit_v = (v[0]/norm_v, v[1]/norm_v)
-
-    # x축 단위 벡터
-    x_unit = (1, 0)
-
-    # 두 벡터의 내적 계산
-    dot_product = x_unit[0]*unit_v[0] + x_unit[1]*unit_v[1]
-
-    # 각도 계산
-    if dot_product == 0:
-        angle = 90
-    else:
-        angle = math.degrees(math.acos(dot_product))
-
-    return angle
+            heading_front = bool(rospy.get_param("~heading_front", True))
 
 def cal_plan(displacement, marker_displacement, aruco_tf):
-    def getEuclidianDistance(new_point, backup_point):
-        return math.hypot(new_point.y - backup_point.y, new_point.x - backup_point.x)
-    
     global robot_odom, backup_pose
     global pub
     if (aruco_tf) != None and robot_odom.header.frame_id != "":
@@ -224,7 +238,7 @@ def cal_plan(displacement, marker_displacement, aruco_tf):
 
         origin_quaternion = aruco_tf.transform.rotation
         origin_translation = (tf_trans.x, tf_trans.y, tf_trans.z)
-        origin_matrix = np.dot(tf.transformations.translation_matrix(origin_translation), tf.transformations.quaternion_matrix\
+        origin_matrix = np.dot(tf.transformations.translation_matrix(origin_translation), quaternion_matrix\
                         ([origin_quaternion.x, origin_quaternion.y, origin_quaternion.z, origin_quaternion.w]))
         
         origin_point = np.array([point_origin.x, point_origin.y, point_origin.z, 1]) 
@@ -242,13 +256,16 @@ def cal_plan(displacement, marker_displacement, aruco_tf):
             robot_odom.pose.pose.orientation.w
         )
         robot_angle = tf.transformations.euler_from_quaternion(q)[2]
-        current_robot_pose = Pose2D(x=robot_odom.pose.pose.position.x, y=robot_odom.pose.pose.position.y, theta=robot_angle) # now robot odom position
-        
         q_tf = (origin_quaternion.x, origin_quaternion.y, origin_quaternion.z, origin_quaternion.w)
         
         tf_angle = tf.transformations.euler_from_quaternion(q_tf)
-        pose_new = Pose2D(x=point_new.x, y=point_new.y, theta=tf_angle[2])
-        origin_pos = Pose2D(x=origin_new[0], y=origin_new[1], theta=tf_angle[2]) # marker origin point(base odom)
+        target_angle = tf_angle[2]
+        # if not heading_front:
+        #     target_angle = normalizeAngle(target_angle + math.pi, -math.pi, math.pi)
+        #     robot_angle = normalizeAngle(robot_angle + math.pi, -math.pi, math.pi)
+        current_robot_pose = Pose2D(x=robot_odom.pose.pose.position.x, y=robot_odom.pose.pose.position.y, theta=robot_angle) # now robot odom position
+        pose_new = Pose2D(x=point_new.x, y=point_new.y, theta=target_angle)
+        origin_pos = Pose2D(x=origin_new[0], y=origin_new[1], theta=target_angle) # marker origin point(base odom)
         sv = math.hypot(robot_odom.twist.twist.linear.x, robot_odom.twist.twist.linear.y)
         result_path = compute_plan(current_robot_pose,
                             pose_new, 
@@ -260,7 +277,7 @@ def cal_plan(displacement, marker_displacement, aruco_tf):
 def sub_TF():
     global callback_trigger, backup_pose
     global tracking_state
-    global pub
+    global pub, polygon_pub, debug_mode
     global cm
     callback_trigger = False
     backup_pose = Point()
@@ -268,17 +285,21 @@ def sub_TF():
     rospy.init_node('subscribeTF')
     cm = CallbackManager()
     cm.register("planning", cal_plan)
-    global path_displacement, robot_size, marker_displacement
+    global path_displacement, robot_size, marker_displacement, heading_front
     path_displacement = float(rospy.get_param("~path_displacement"))
     robot_size = float(rospy.get_param("~robot_size"))
     marker_displacement = float(rospy.get_param("~marker_displacement"))
+    heading_front = bool(rospy.get_param("~heading_front", True))
+    debug_mode = bool(rospy.get_param("~debug_mode", False))
     print("path_displacement : ", path_displacement)
     print("robot_size : ", robot_size)
     print("marker_displacement : ", marker_displacement)
+    print("heading_front : ", heading_front)
     rospy.Subscriber("filtered_tf", TransformStamped, callback_arucoTF)
     rospy.Subscriber("odom", Odometry, callback_odomTF)
     rospy.Subscriber("docking/status", String, callback_tracking_state)
     pub = rospy.Publisher('compute_Path', Path, queue_size=1)
+    polygon_pub = rospy.Publisher("angle_check", PolygonStamped, queue_size=1)
     rospy.spin()
     
 
